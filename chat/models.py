2693fd4aeb9c36144fc241433778ae35bf96b3f7
mahdi <i.mahdikhanmohammadi@gmail.com>
2026-06-23 13:48:03 +0330

    chat models: create models for chat app and register models in admin.py

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django_jalali.db import models as jmodels
 
 # Create your models here.
+
+
+class Room(models.Model):
+    users = models.ManyToManyField("accounts.Profile")
+    created_date = jmodels.jDateTimeField(auto_now_add=True)
+    updated_date = jmodels.jDateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"room: {self.pk}"
+
+    class Meta:
+        ordering = ["-created_date", '-updated_date']
+
+
+class Message(models.Model):
+    author = models.ForeignKey("accounts.Profile", on_delete=models.PROTECT)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    text = models.TextField()
+    send_date = jmodels.jDateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username}: {self.text[:10]}"
+
+    class Meta:
+        ordering = ["-send_date"]
+
+
+class Contact(models.Model):
+    owner = models.ForeignKey("accounts.Profile", on_delete=models.CASCADE)
+    username = models.CharField(max_length=200)
+    created_date = jmodels.jDateTimeField(auto_now_add=True)
+    updated_date = jmodels.jDateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"owner: {self.owner.username} - {self.username}"
+
+    class Meta:
+        ordering = ["-created_date", '-updated_date']

9d48b2a76abd224b23463180fc35df991b336e08
mahdi <i.mahdikhanmohammadi@gmail.com>
2026-06-25 13:14:24 +0330

    consumer: coonent consumer , channel layer create webscoket script update requirements for docker finaly channles work succesfuly

--- chat/webSocket/consumers.py
+++ chat/webSocket/consumers.py
@@ -1,50 +1,62 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 from django.utils import timezone
 from chat.models import Message, Room
 from accounts.models import Profile
-from asgiref.sync import async_to_sync
+from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_id = self.scope["url_route"]["kwargs"]["pk"]
         self.room_group_name = f"chat_{self.room_id}"
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
 
         current_user = self.scope["user"]
-        Profile.objects.filter(user=current_user).update(is_online=True)
 
-        self.accept()
+        await sync_to_async(Profile.objects.filter(user=current_user).update)(is_online=True)
 
-    def disconnect(self, code):
+        await self.accept()
+
+    async def disconnect(self, code):
         current_user = self.scope["user"]
-        Profile.objects.filter(user=current_user).update(
+        await sync_to_async(Profile.objects.filter(user=current_user).update)(
             is_online=False, last_seen=timezone.now())
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         current_user = self.scope["user"]
-        current_room = Room.objects.get(pk=self.scope["url_route"]["kwargs"]["pk"]
-                                        )
+        current_room = await database_sync_to_async(Room.objects.get)(pk=self.scope["url_route"]["kwargs"]["pk"]
+                                                                      )
 
-        Message.objects.create(author=current_user.profile,
-                               room=current_room, text=message)
+        profile = await database_sync_to_async(lambda: current_user.profile)()
 
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
+        await database_sync_to_async(Message.objects.create)(author=profile,
+                                                             room=current_room, text=message)
+
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat.message",
+                                   "message": {
+                                       'text': message, 'author_id': current_user.profile.pk,
+                                       'profile_image': current_user.profile.image_profile.url
+                                   },
+                                   },
         )
 
-    def chat_message(self, event):
-        message = event["message"]
+    async def chat_message(self, event):
+        message = event["message"]['text']
+        author_id = event['message']['author_id']
+        profile_image = event['message']['profile_image']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        await self.send(text_data=json.dumps(
+            {'message': message, 'author_id': author_id, 'profile_image': profile_image}))
